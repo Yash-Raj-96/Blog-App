d564370020d5f7ee5f97380d7c34a92ca6df6119
Yash-Raj-96 <166122718+Yash-Raj-96@users.noreply.github.com>
2025-04-05 10:36:14 +0530

    urls.py

--- backend/blog_project/urls.py
+++ backend/blog_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/blogs/', include('blogs.urls')),  # 👈 Prefix added
-    path('api/accounts/', include('accounts.urls')),  # 👈 Prefix added
+    path('', include('blogs.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
